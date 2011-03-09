ddda73a275c18026d0762494d63b3c87a4734843
dgardner88 <david.gardner@students.olin.edu>
2011-03-08 20:06:05 -0500

    added project to rubymine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkyhookSprintRails::Application.config.secret_token = 'd6e53558fb6afb33555623a61979ece58403249e6fc4d336c846137eb1601db5f1cff68f01c2cbca31d99d9fa24bb41afa7ec8dbf0200512eb0ff1b742e2ac02'
