ce051166853efeba4dba0287420d0aa54b6c95bf
dgardner88 <david.gardner@students.olin.edu>
2011-03-10 13:56:22 -0500

    round and round

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,11 +49,11 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-        format.json
+        format.json { render :json => @user, :status => :created, :location => @user }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
-        format.json
+        format.json { render :json => @user.errors, :status => :unprocessable_entity }
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
     end
