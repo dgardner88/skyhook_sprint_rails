120aff358d528c97fe726edc6d43b295821b5bd5
dgardner88 <david.gardner@students.olin.edu>
2011-03-10 13:44:37 -0500

    trying to get user push to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,12 +49,12 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
+        format.json
         format.xml  { render :xml => @user, :status => :created, :location => @user }
-        format.json { render :json => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
+        format.json
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-        format.json  { render :json => @user.errors, :status => :unprocessable_entity }
       end
     end
   end
